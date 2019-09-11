26d701c66712bf6158b08941403eb43af4ec7ea9
Hardeep Dhaliwal <hdhaliwal@thinkful.com>
2019-09-10 17:26:32 -0700

    first commit

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
+$('#js-shopping-list-form').submit(function(event) {
+  event.preventDefault();
+  let newItem = $(this).find('.js-shopping-list-entry').val();
+  addNewItem(newItem);
+});
 
-$(function addListItem(){
-    $('#js-shopping-list-form').submit(event => {
-        event.preventDefault();
+$('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+  $(this).closest("li").remove();
+});
 
-        const userText = $('.js-shopping-list-entry').val();
+$('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+  $(this).closest("li").find('.shopping-item').toggleClass('shopping-item__checked');
+});
 
-        $('.shopping-list').append(`<li><span class="shopping-item">${userText}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class = "shopping-item-delete"><span class= "button-label">delete</span></button></div>`); 
-        
-    })
-})
-
-$(function toggleCheck() {
-    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-        $(this).closest('li').find($('.shopping-item')).toggleClass("shopping-item__checked");
-    })
-})
-// This was my first code to toggle the check. 
-// $(function toggleCheck() {
-//     $('.shopping-item-toggle').on('click', => {
-//         $(this).closest(li).find($('.shopping-item')).toggleClass("shopping-item__checked");
-//     })
-// })
-
-$(function removeItem(){
-    $('.shopping-list').on('click','.shopping-item-delete', function(event) {
-        $(this).closest('li').remove()
-    })
-})
+function addNewItem(item) {
+  $('.shopping-list').append(
+    `<li>
+        <span class="shopping-item">${item}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`
+  );
+}
